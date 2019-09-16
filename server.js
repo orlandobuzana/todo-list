1bb9e46dc5301660d70de141054bc3855846bf0e
Orlando Buzana <orlandobuzana@outlook.com>
2019-09-16 01:40:34 -0400

    on index.js added lines 1 and 97 wrapper

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 var SQL_DB = process.env.SQL_DB;
 var SQL_PASS = process.env.SQL_PASS;
 var SQL_USER = process.env.SQL_USER;
-var SQL_HOST = process.env.SQL_HOST
+var SQL_HOST = process.env.SQL_HOST;
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
